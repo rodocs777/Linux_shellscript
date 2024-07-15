46bc61d7d861e6d113d9a7de1e758990bef01400
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-07-15 13:13:12 -0300

    commit

--- deployweb.sh
+++ deployweb.sh
@@ -8,18 +8,26 @@
 # Ambiente  : homologação                                                                                #
 # Autor     : Rodolfo F. Mariucci                                                                        #
 # Criação   : 28/03/2024                                                                                 #
-# Alteracao : 20/05/2024                                                                                 #
+# Alteracao : 03/06/2024                                                                                 #
 ##########################################################################################################
 
 #==========> Variaveis de Ambiente <==========#
+
+mkdir -p Logs/
+chmod 777 Logs
+mkdir -p homologa/
+chmod 777 homologa
 
 nomeshell="deployweb"
 DataHora=$(date "+%Y%m%d%H%M%S")
 caminhoShell=$(pwd)
 caminhoLog=$caminhoShell/Logs/
 arquivoLog="${caminhoLog}${nomeshell}.${DataHora}.log"
-cd /home/a4882yd/homologa/
-chmod 777 *
+caminhoBin=$caminhoShell/homologa/
+
+cd $caminhoBin 
+chmod -R 777 * $caminhoBin 
+
 
 INSTANCIAS=("$@")
 
@@ -27,7 +35,8 @@ INSTANCIAS=("$@")
 
 deploy_instance() {
   local INSTANCIA=$1
-
+  
+  echo "" 
   echo "" 															>>$arquivoLog
   echo "=================== Script de Deploy Web - $INSTANCIA ===============" >>$arquivoLog
   echo ""															>>$arquivoLog
@@ -47,19 +56,20 @@ deploy_instance() {
   echo "" >>$arquivoLog
   echo "" >>$arquivoLog
   sudo -u admweb /app/scripts/webctl $INSTANCIA stop  >>$arquivoLog
+  sleep 30
   cd /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/
   sudo -u admweb rm -r data
   sudo -u admweb rm -r tmp
-  sudo -u admweb cp -prf /home/a4882yd/homologa/CMS-* /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/
+  sudo -u admweb cp -prf $caminhoBin/CMS-* /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/
   sudo -u admweb rm /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/*.failed
   sudo -u admweb rm /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/*.dodeploy
   sudo -u admweb rm /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/*.deployed
   sudo -u admweb rm /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/*.isdeploying
   sudo -u admweb rm /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/*.undeployed
   sudo -u admweb /app/scripts/webctl $INSTANCIA start
+  sleep 30
   sudo -u admweb /app/scripts/webctl $INSTANCIA status >>$arquivoLog
   cd /app/jboss/paywareweb-ws/jboss-eap-7.0/$INSTANCIA/deployments/
-  sleep 3
   ls -l >>$arquivoLog
   echo "" >>$arquivoLog
   echo "O numero de 'faileds' DEPOIS de executar o script são: " >>$arquivoLog
@@ -70,6 +80,10 @@ deploy_instance() {
   echo "" >>$arquivoLog
   echo "Os PIDs:"  >>$arquivoLog
   ps -ef | grep $INSTANCIA >>$arquivoLog
+  echo "" 
+  echo ""
+  echo ""
+  echo ""
 }
 
 #==========> Loop através das instâncias <==========#
