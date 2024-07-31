3efbb33d7278f13686098d86d012cb01a03d5e59
Rodolfo <105673932+rodocs777@users.noreply.github.com>
2024-07-31 17:00:31 -0300

    melhoria
    deixei a shell com log e mais orientada a objetos, para executar com um parâmetro a menos e ter o menos resultado.

--- batchsdeploy.sh
+++ batchsdeploy.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 #######################################################################################################
 # Descricao : Deploy de binários batchs Acqr
-#               IMPORTANTE: antes de entrar com o owner pelo menu, dar permissão chmod 777 no binarios com seu owner pessoal.
-#
-# Exemplo de execucao, após ter logao ao ownerda instancia, com o menu
-# "./batchsdeploy.sh acqr acqr 01"      ou       "./batchsdeploy.sh issr issr 01"  (01, 02, 03, 04, 05, 06, 07, 08, 09 ou 10)
-#                         /appl/CMS/cmsacqr09/CMS/bin
-# Data      : 02/04/2024
-# Autor     : Rodolfo Ferraz
-# Alteracao :
+# Local de execução: cd /tmp/deploy/           
+# Exemplo de execucao (após ter logado ao owner da instancia com o menu dos ambientes):
+# cd /tmp/deploy/
+# "./batchsdeploy.sh acqr acqr 01" 	ou	 "./batchsdeploy.sh issr 01"  (01, 02, 03, 04, 05, 06, 07, 08, 09 ou 10)
+# 			  /appl/CMS/cmsacqr09/CMS/bin
+# Data: 02/04/2024
+# Autor: Rodolfo Ferraz
+# Alteracao: 14/06/2024
 #######################################################################################################
 
+nomeshell="batchsdeploy"
+DataHora=$(date "+%Y%m%d%H%M%S")
+caminhoShell=/tmp/deploy
+caminhoLog=/tmp/deploy/logs/
+arquivoLog="${caminhoLog}${nomeshell}.${DataHora}.log"
 
 INSTANCIA1=$1
 INSTANCIA2=$2
-INSTANCIA3=$3
+
+cd /tmp/deploy/$INSTANCIA1
+
+# Copia os arquivos para o destino
+cp * /appl/CMS/cms$INSTANCIA1$INSTANCIA2/CMS/bin >>$arquivoLog 2>&1
+
+cd /appl/CMS/cms$INSTANCIA1$INSTANCIA2/CMS/bin
 
 
-(cd /tmp/deploy/$INSTANCIA1;
-cp * /appl/CMS/cms$INSTANCIA2$INSTANCIA3/CMS/bin)
+# Exibe os novos arquivos e adiciona ao log
+echo "Novos arquivos copiados:" >>$arquivoLog
 
+
+# Exibe a lista final de arquivos no destino e adiciona ao log
+ls -lt>>$arquivoLog
